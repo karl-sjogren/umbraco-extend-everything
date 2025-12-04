04de428e45ff5dd3629204488ad807469144858d
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2025-12-05 00:11:18 +0100

    Added theme extension

--- src/ExtendEverything/Client/src/bundle.manifests.ts
+++ src/ExtendEverything/Client/src/bundle.manifests.ts
@@ -5,6 +5,7 @@ import MediaCollectionAction from './ee-media-collection-action/manifest';
 import RemoveNewsDashboard from './ee-remove-news-dashboard/manifest';
 import SettingsDialog from './ee-settings-dialog/manifest';
 import MainEntryPoint from './ee-entrypoint/manifest';
+import Theme from './ee-theme/manifest';
 
 // Job of the bundle is to collate all the manifests from different parts of the extension and load other manifests
 // We load this bundle from umbraco-package.json
@@ -15,5 +16,6 @@ export const manifests: Array<UmbExtensionManifest> = [
   ...RemoveNewsDashboard,
   ...SettingsDialog,
   ...MainEntryPoint,
-  ...Localization
+  ...Localization,
+  ...Theme
 ];
---
+++ src/ExtendEverything/Client/src/ee-theme/manifest.ts
@@ -0,0 +1,9 @@
+export default [
+  {
+    name: "Extend Everything Theme",
+    alias: "ExtendEverything.Theme",
+    type: "theme",
+    css: "/App_Plugins/ExtendEverything/extend-everything.theme.css",
+    weight: 150,
+  } as UmbExtensionManifest
+];
